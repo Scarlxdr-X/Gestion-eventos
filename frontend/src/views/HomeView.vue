34b2921f0e641b0e60ed1318666670f6a53fef3c
Scarlxdr-X <camposadrian447@gmail.com>
2026-03-23 00:13:44 +0000

    conexion frontend y backend lista

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ mensaje }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '../api'
+
+const mensaje = ref('Cargando...')
+
+onMounted(async () => {
+  try {
+    const respuesta = await api.get('/')
+    mensaje.value = respuesta.data.mensaje
+  } catch (error) {
+    mensaje.value = 'Error conectando con el backend'
+  }
+})
+</script>
